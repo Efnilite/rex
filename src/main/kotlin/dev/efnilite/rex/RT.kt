cde023b5ffc830cd132abf66c93b41e22474a6ef
Efnilite <re.verstappen@gmail.com>
2024-06-20 23:39:09 +0200

    Added *, /, -

--- src/main/kotlin/dev/efnilite/rex/RT.kt
+++ src/main/kotlin/dev/efnilite/rex/RT.kt
@@ -40,4 +40,121 @@ object RT {
             }
         }
     }
+
+    fun subtract(a: Any?, b: Any?): Number {
+        return when (a) {
+            is Int -> {
+                when (b) {
+                    is Int -> a - b
+                    is Double -> a - b
+                    is Long -> a - b
+                    else -> a - b.toString().toDouble()
+                }
+            }
+            is Double -> {
+                when (b) {
+                    is Int -> a - b
+                    is Double -> a - b
+                    is Long -> a - b
+                    else -> a - b.toString().toDouble()
+                }
+            }
+            is Long -> {
+                when (b) {
+                    is Int -> a - b
+                    is Double -> a - b
+                    is Long -> a - b
+                    else -> a - b.toString().toLong()
+                }
+            }
+            else -> {
+                val parsedA = a.toString().toDouble()
+
+                when (b) {
+                    is Int -> parsedA - b
+                    is Double -> parsedA - b
+                    is Long -> parsedA - b
+                    else -> parsedA - b.toString().toDouble()
+                }
+            }
+        }
+    }
+
+    fun multiply(a: Any?, b: Any?): Number {
+        return when (a) {
+            is Int -> {
+                when (b) {
+                    is Int -> a * b
+                    is Double -> a * b
+                    is Long -> a * b
+                    else -> a * b.toString().toDouble()
+                }
+            }
+            is Double -> {
+                when (b) {
+                    is Int -> a * b
+                    is Double -> a * b
+                    is Long -> a * b
+                    else -> a * b.toString().toDouble()
+                }
+            }
+            is Long -> {
+                when (b) {
+                    is Int -> a * b
+                    is Double -> a * b
+                    is Long -> a * b
+                    else -> a * b.toString().toLong()
+                }
+            }
+            else -> {
+                val parsedA = a.toString().toDouble()
+
+                when (b) {
+                    is Int -> parsedA * b
+                    is Double -> parsedA * b
+                    is Long -> parsedA * b
+                    else -> parsedA * b.toString().toDouble()
+                }
+            }
+        }
+    }
+
+    fun divide(a: Any?, b: Any?): Number {
+        return when (a) {
+            is Int -> {
+                when (b) {
+                    is Int -> a / b.toDouble()
+                    is Double -> a / b
+                    is Long -> a / b
+                    else -> a / b.toString().toDouble()
+                }
+            }
+            is Double -> {
+                when (b) {
+                    is Int -> a / b
+                    is Double -> a / b
+                    is Long -> a / b
+                    else -> a / b.toString().toDouble()
+                }
+            }
+            is Long -> {
+                when (b) {
+                    is Int -> a / b
+                    is Double -> a / b
+                    is Long -> a / b
+                    else -> a / b.toString().toLong()
+                }
+            }
+            else -> {
+                val parsedA = a.toString().toDouble()
+
+                when (b) {
+                    is Int -> parsedA / b
+                    is Double -> parsedA / b
+                    is Long -> parsedA / b
+                    else -> parsedA / b.toString().toDouble()
+                }
+            }
+        }
+    }
 }
--- src/test/kotlin/RTTest.kt
+++ src/test/kotlin/RTTest.kt
@@ -15,4 +15,39 @@ object RTTest {
         assertEquals(10E33, RT.add(5E33, 5E33))
     }
 
+    @Test
+    fun testSubtract() {
+        assertEquals(-1, RT.subtract(1, 2))
+        assertEquals(-1.0, RT.subtract(1.0, 2))
+        assertEquals(3, RT.subtract(1, -2))
+        assertEquals(-3.0, RT.subtract(1, 4.0))
+        assertEquals(3.0, RT.subtract(-1, -4.0))
+        assertEquals(-7.0, RT.subtract("-5", 2.0))
+        assertEquals(-8.5, RT.subtract(-3.5, "5.0"))
+        assertEquals(0.0, RT.subtract(1E30, 1E30))
+    }
+
+    @Test
+    fun testMultiply() {
+        assertEquals(2, RT.multiply(1, 2))
+        assertEquals(2.0, RT.multiply(1.0, 2))
+        assertEquals(-2, RT.multiply(1, -2))
+        assertEquals(-2.0, RT.multiply(1, -2.0))
+        assertEquals(-2.0, RT.multiply(-1, 2.0))
+        assertEquals(-10.0, RT.multiply("-5", 2.0))
+        assertEquals(-17.5, RT.multiply(-3.5, "5.0"))
+        assertEquals(1E20, RT.multiply(1E10, 1E10))
+    }
+
+    @Test
+    fun testDivide() {
+        assertEquals(0.5, RT.divide(1, 2))
+        assertEquals(0.5, RT.divide(1.0, 2))
+        assertEquals(-0.5, RT.divide(1, -2))
+        assertEquals(-0.5, RT.divide(1, -2.0))
+        assertEquals(-0.5, RT.divide(-1, 2.0))
+        assertEquals(-2.5, RT.divide("-5", 2.0))
+        assertEquals(-0.7, RT.divide(-3.5, "5.0"))
+        assertEquals(10.0, RT.divide(5E33, 5E32))
+    }
 }
