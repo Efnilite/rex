9a8ac6e4c2e43c830d64a79079fbe8bfb3945676
Efnilite <35348263+Efnilite@users.noreply.github.com>
2024-07-03 01:20:59 +0200

    Added core reading on REPL start

--- src/main/kotlin/dev/efnilite/rex/REPL.kt
+++ src/main/kotlin/dev/efnilite/rex/REPL.kt
@@ -5,28 +5,39 @@ import dev.efnilite.rex.Tokenizer.Companion.tokenize
 
 object REPL {
 
-    private val scope = Scope(null)
-
     @JvmStatic
     fun main(args: Array<String>) {
         main()
     }
 
     private fun main() {
+        val scope = Scope(null)
+
+        eval(readCore(), scope)
+
         while (true) {
             print("rex> ")
-            val input = readlnOrNull() ?: break
+            val input = readlnOrNull()?.trim() ?: break
 
-            if (input.lowercase() == "(exit)") break
-
-            try {
-                val tokens = tokenize(input)
-                val result = parse(tokens, scope)
-
-                println(result)
-            } catch (e: Exception) {
-                System.err.println(e.message)
+            if (input.isNotEmpty()) {
+                eval(input, scope)
             }
+        }
+    }
+
+    private fun readCore(): String {
+        return this::class.java.getResourceAsStream("/core.rx")!!.bufferedReader().use { it.readText() }
+    }
+
+    private fun eval(input: String, scope: Scope) {
+        try {
+            val tokens = tokenize(input)
+            val result = parse(tokens, scope)
+
+            println(result)
+        } catch (e: Exception) {
+            e.printStackTrace()
+            println()
         }
     }
 }
